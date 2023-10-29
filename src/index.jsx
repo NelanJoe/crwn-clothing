248034778bc8575fad2419f022a147945912e0f8
NelanJoe <nelanjorelat@gmail.com>
2023-10-29 17:04:13 +0700

    ✅ create: checkout page along with checkout items and chekout cart functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -12,15 +11,13 @@ import "./index.scss";
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <ProductsProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </ProductsProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </BrowserRouter>
 );
