735bca333845ab0f84da58e5328cc75bd988a6ab
NelanJoe <nelanjorelat@gmail.com>
2023-10-31 20:45:37 +0700

    🎨 migrate from context to redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import { GlobalStyle } from "./global.styles";
 
@@ -12,9 +13,11 @@ const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
